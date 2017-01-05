95b1dfc51871b3613c63d2804c1e9ab1974aaa2d
sebres <info@sebres.de>
2017-01-05 16:54:59 +0100

    win (x86) binaries to test new tcl-clock engine (clock scan, clock format, clock add)

--- get-library.cmd
+++ get-library.cmd
@@ -1,5 +1,5 @@
 @cd /d %~dp0
 
-git clone https://github.com/sebres/tcl.git -b sb/_se-back-port/timerate .\tcl8.7
+git clone https://github.com/sebres/tcl.git -b sb/trunk-rewrite-clock-in-c .\tcl8.7
 
 mklink /J library %~dp0\tcl8.7\library
